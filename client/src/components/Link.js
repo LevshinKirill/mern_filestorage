8e1f46e164752b954190f671ff9ec60819540615
kirusha <levshinkirill00@gmail.com>
2021-12-17 21:42:33 +0300

    added react-copy-to-clipboard package and added build folder to gitignore

--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -1,14 +1,14 @@
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
 const Link = ({ link, setLink }) => (
   <div className='card shadow-lg'>
     <div className='card-body p-5 d-flex flex-column justify-content-center align-items-center'>
       <h2>Download link</h2>
       <p>The files was shared via mern_filestorage and you can download your files by link</p>
       <p><strong>Link: </strong>{link}</p>
-      <button
-        className='btn btn-lg btn-primary'
-        onClick={() => navigator.clipboard.writeText(link)}>
-        Copy link
-      </button>
+      <CopyToClipboard text={link}>
+        <button className='btn btn-lg btn-primary'>Copy link</button>
+      </CopyToClipboard>
       <button
         className='btn btn-link'
         onClick={() => setLink(null)}>
